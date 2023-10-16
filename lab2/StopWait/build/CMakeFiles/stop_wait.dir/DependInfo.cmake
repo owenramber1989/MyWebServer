b276c32c99f27be297a204924010b2cf6517369b
Ubuntu <owenramber1989@owenramber1989.yldo0uc2fq5uzege00tjiujouf.lx.internal.cloudapp.net>
2023-10-16 13:05:12 +0000

    <docs>: <tweaked the structure>

---
+++ lab2/StopWait/build/CMakeFiles/stop_wait.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/owenramber1989/MyWebServer/lab2/Ubuntu_18.04.3_64_Desktop/StopWait/src/StopWait.cpp" "CMakeFiles/stop_wait.dir/src/StopWait.cpp.o" "gcc" "CMakeFiles/stop_wait.dir/src/StopWait.cpp.o.d"
+  "/home/owenramber1989/MyWebServer/lab2/Ubuntu_18.04.3_64_Desktop/StopWait/src/StopWaitRdtReceiver.cpp" "CMakeFiles/stop_wait.dir/src/StopWaitRdtReceiver.cpp.o" "gcc" "CMakeFiles/stop_wait.dir/src/StopWaitRdtReceiver.cpp.o.d"
+  "/home/owenramber1989/MyWebServer/lab2/Ubuntu_18.04.3_64_Desktop/StopWait/src/StopWaitRdtSender.cpp" "CMakeFiles/stop_wait.dir/src/StopWaitRdtSender.cpp.o" "gcc" "CMakeFiles/stop_wait.dir/src/StopWaitRdtSender.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab2/StopWait/build/CMakeFiles/stop_wait.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/stop_wait"
+  "../bin/stop_wait.pdb"
+  "CMakeFiles/stop_wait.dir/src/StopWait.cpp.o"
+  "CMakeFiles/stop_wait.dir/src/StopWait.cpp.o.d"
+  "CMakeFiles/stop_wait.dir/src/StopWaitRdtReceiver.cpp.o"
+  "CMakeFiles/stop_wait.dir/src/StopWaitRdtReceiver.cpp.o.d"
+  "CMakeFiles/stop_wait.dir/src/StopWaitRdtSender.cpp.o"
+  "CMakeFiles/stop_wait.dir/src/StopWaitRdtSender.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stop_wait.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
